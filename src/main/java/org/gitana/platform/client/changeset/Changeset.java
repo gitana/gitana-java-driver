8653b2d5797f4068a60b6ac6a9b2cba80b500e2b
uzquiano <mgu1@cornell.edu>
2017-03-02 22:32:00 -0500

    Updated license file

--- src/main/java/org/gitana/platform/client/changeset/Changeset.java
+++ src/main/java/org/gitana/platform/client/changeset/Changeset.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 Gitana Software, Inc.
+ * Copyright 2017 Gitana Software, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
  * For more information, please contact Gitana Software, Inc. at this
  * address:
  *
- *   info@gitanasoftware.com
+ *   info@cloudcms.com
  */
 
 package org.gitana.platform.client.changeset;
